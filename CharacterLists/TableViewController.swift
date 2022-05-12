9c6453d576cce241e6b9c3810a883b06c6a56ad4
aitkazindev <aitkazindev@gmail.com>
2022-05-13 05:00:55 +0900

    added function for deleting from list

--- CharacterLists/TableViewController.swift
+++ CharacterLists/TableViewController.swift
@@ -76,17 +76,21 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            arrayNames.remove(at: indexPath.row)
+            arrayImages.remove(at: indexPath.row)
+            arrayLastNames.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
